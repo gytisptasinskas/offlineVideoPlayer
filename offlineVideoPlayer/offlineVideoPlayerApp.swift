284a8fbda24cd079fc3d3d4d570c9f0e9d223448
Gytis Ptasinskas <ptasinskasgytis@gmail.com>
2024-02-14 10:31:52 +0200

    Initial Commit

---
+++ offlineVideoPlayer/offlineVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  offlineVideoPlayerApp.swift
+//  offlineVideoPlayer
+//
+//  Created by Gytis Ptasinskas on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct offlineVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
